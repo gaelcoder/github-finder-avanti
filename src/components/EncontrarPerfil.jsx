71fdfc5523bdcb1db9e85b2610669ae587fc69f4
Gabriel Lima de Souza Azevedo <gabriel.lazevedo@al.infnet.edu.br>
2025-04-17 09:08:16 -0300

    últimas alterações do sistema, projeto finalizado

--- src/components/EncontrarPerfil.jsx
+++ src/components/EncontrarPerfil.jsx
@@ -2,10 +2,10 @@ import { useState, useEffect, use } from "react";
 import styles from "./EncontrarPerfil.module.css";
 import axios from "axios";
 
-function EncontrarPerfil(){
-    const [busca, setBusca] = useState('');
-    const [resultados, setResultados] = useState([]);
-    const [semResultados, setSemResultados] = useState(false)
+function EncontrarPerfil() {
+  const [busca, setBusca] = useState('');
+  const [resultados, setResultados] = useState([]);
+  const [semResultados, setSemResultados] = useState(false)
 
   const buscarUsuarios = () => {
     if (!busca.trim()) {
@@ -50,40 +50,42 @@ function EncontrarPerfil(){
   };
 
 
-    return(
-        <>
-            <section className={styles.modal}>
-                <figure>
-                    <img src="https://img.icons8.com/ios11/512/FFFFFF/github.png" width="80px"/>
-                    <h2>Perfil <a>GitHub</a></h2>
-                </figure>
-                <div className={styles.barraBusca}>
-                <input className={styles.campo} type="text" placeholder="Buscar usuário do github..."
-                value={busca} 
-                onChange={(e) => setBusca(e.target.value)}
-                />
-                 <button onClick={buscarUsuarios} className={styles.botaoBuscar}><img src="https://img.icons8.com/ios7/512/FFFFFF/search.png" width="20px"/></button>
-                 </div>
-            <div className={styles.listaUsuarios}>
-            {resultados.map(user => (
-             
-                    <a key={user.id}
-                    href={user.html_url}
-                    target="_blank"
-                    rel="noreferrer"
-                    className={styles.card}
-                    >
-                        <img src={user.avatar_url} alt={user.login} className={styles.avatar}/>
-                        <h3>{user.login}</h3>
-                        <p>{user.bio || "Sem bio disponível"}</p>
-                    </a>
-                ))}
-
-                {semResultados && <p>Nenhum usuário encontrado</p>}
-                </div>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section className={styles.modal}>
+        <figure>
+          <img src="https://img.icons8.com/ios11/512/FFFFFF/github.png" width="80px" />
+          <h2>Perfil <a>GitHub</a></h2>
+        </figure>
+        <div className={styles.barraBusca}>
+          <input className={styles.campo} type="text" placeholder="Buscar usuário do github..."
+            value={busca}
+            onChange={(e) => setBusca(e.target.value)}
+          />
+          <button onClick={buscarUsuarios} className={styles.botaoBuscar}><img src="https://img.icons8.com/ios7/512/FFFFFF/search.png" width="20px" /></button>
+        </div>
+        <div className={styles.listaUsuarios}>
+          {resultados.map(user => (
+            <a key={user.id}
+              href={user.html_url}
+              target="_blank"
+              rel="noreferrer"
+              className={styles.cardUsuario}
+            >
+              <img src={user.avatar_url} alt={user.login} className={styles.avatar} />
+              <div className={styles.infoUsuario}>
+                <h3>{user.login}</h3>
+                <p>{user.bio || "Sem bio disponível"}</p>
+              </div>
+            </a>
+          ))}
+          {semResultados && <div className={styles.alerta}>
+            <p>Nenhum perfil foi encontrado com esse nome de usuário. Tente novamente.</p>
+          </div>}
+        </div>
+      </section>
+    </>
+  );
 
 }
 
